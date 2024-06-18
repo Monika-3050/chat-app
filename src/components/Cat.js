33b76650e2c526b939c2c19f3528098cfdab225b
Monika <moni.ca264173@gmail.com>
2024-06-18 21:17:04 +0530

    -created ui for chat sidebar -created ui for chat box -created ui for messages

---
+++ src/components/Cat.js
@@ -0,0 +1,10 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+
+const Cat = () => {
+  return (
+    <Avatar src="https://c.ndtvimg.com/2023-03/0m65kep_samosa_625x300_10_March_23.jpg"/>
+  )
+}
+
+export default Cat
--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,11 +1,28 @@
+
 import React from "react";
+import Chats from "./Chats";
+import { Stack , Box} from "@mui/material";
+import Conversation from "../../components/Conversation";
+import { useTheme } from '@mui/material/styles';
 
 const GeneralApp = () => {
-
+  
+  const theme=useTheme();
+  
   return (
-    <>
-      App
-    </>
+    <Stack direction={"row"} sx={{ width:"100%"}}>
+      {/*chats*/}
+      <Chats/>
+       
+       <Box 
+        sx={{
+        height:"100%" , 
+        width:" calc(100vw - 420px) " , 
+        backgroundColor:theme.palette.mode==="light" ? "#fff" : theme.palette.background.default}}>
+
+        <Conversation/>
+       </Box>
+    </Stack>
   );
 };
 
