2caa277f7c0c00a74b37ab048de4fee96169de1d
Monika <moni.ca264173@gmail.com>
2024-06-23 18:46:35 +0530

    created group chat functionality

--- src/pages/dashboard/Chats.js
+++ src/pages/dashboard/Chats.js
@@ -1,108 +1,25 @@
-import { Box, IconButton, Stack, Typography, InputBase, Button, Divider, Avatar, Badge } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  Stack,
+  Typography,
+  Button,
+  Divider,
+} from "@mui/material";
 import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
-import { styled, alpha, useTheme } from "@mui/material/styles";
+import {  useTheme } from "@mui/material/styles";
 import React from "react";
-import { faker } from "@faker-js/faker";
 import { ChatList } from "../../data";
-import SimpleBar from 'simplebar-react'; // Import SimpleBar if it's a custom scrollbar component
-import 'simplebar/dist/simplebar.min.css'; // Import SimpleBar CSS if necessary
+import SimpleBar from "simplebar-react"; // Import SimpleBar if it's a custom scrollbar component
+import "simplebar/dist/simplebar.min.css"; // Import SimpleBar CSS if necessary
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+} from "../../components/Search";
+import ChatElement from "../../components/ChatElement";
 
-const StyledBadge = styled(Badge)(({ theme }) => ({
-  '& .MuiBadge-badge': {
-    backgroundColor: '#44b700',
-    color: '#44b700',
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    '&::after': {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      animation: 'ripple 1.2s infinite ease-in-out',
-      border: '1px solid currentColor',
-      content: '""',
-    },
-  },
-  '@keyframes ripple': {
-    '0%': {
-      transform: 'scale(.8)',
-      opacity: 1,
-    },
-    '100%': {
-      transform: 'scale(2.4)',
-      opacity: 0,
-    },
-  },
-}));
 
-const ChatElement = ({ id, name, img, msg, time, unread, online }) => {
-    const theme = useTheme();
-  return (
-    <Box sx={{
-      width: "100%",
-      borderRadius: 1,
-      backgroundColor: theme.palette.mode === "light" ? "#fff" : theme.palette.background.default,
-    }}
-      p={2}
-      key={id} // Add key prop
-    >
-      <Stack direction={"row"} alignItems={"center"} justifyContent={"space-between"}>
-        <Stack direction={"row"} spacing={2}>
-          {online ? <StyledBadge
-            overlap="circular"
-            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-            variant="dot"
-          >
-            <Avatar src={faker.image.avatar()} />
-          </StyledBadge> : <Avatar src={faker.image.avatar()} />}
-          <Stack spacing={0.3}>
-            <Typography variant="subtitle2">
-              {name}
-            </Typography>
-            <Typography variant="caption">
-              {msg}
-            </Typography>
-          </Stack>
-        </Stack>
-        <Stack spacing={2} alignItems={"center"}>
-          <Typography sx={{ fontWeight: 600 }} variant="caption">
-            {time}
-          </Typography>
-          {unread > 0 && <Badge color="primary" badgeContent={unread} />} {/* Conditionally render Badge */}
-        </Stack>
-      </Stack>
-    </Box>
-  )
-}
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: 20,
-  backgroundColor: alpha(theme.palette.background.default, 1),
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    width: "100%",
-  },
-}));
 
 const Chats = () => {
   const theme = useTheme();
@@ -111,15 +28,20 @@ const Chats = () => {
       sx={{
         position: "relative",
         width: 320,
-        backgroundColor: theme.palette.mode === "light" ? "#F8FAFF" : theme.palette.background.paper,
+        backgroundColor:
+          theme.palette.mode === "light"
+            ? "#F8FAFF"
+            : theme.palette.background.paper,
         boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
       }}
     >
       <Stack p={3} spacing={2} sx={{ height: "100vh" }}>
-        <Stack direction="row" alignItems={"center"} justifyContent="space-between">
-          <Typography variant="h5">
-            Chats
-          </Typography>
+        <Stack
+          direction="row"
+          alignItems={"center"}
+          justifyContent="space-between"
+        >
+          <Typography variant="h5">Chats</Typography>
           <IconButton>
             <CircleDashed />
           </IconButton>
@@ -129,40 +51,44 @@ const Chats = () => {
             <SearchIconWrapper>
               <MagnifyingGlass color="#709CE6" />
             </SearchIconWrapper>
-            <StyledInputBase placeholder="Search..." inputProps={{ "aria-label": "search" }} />
+            <StyledInputBase
+              placeholder="Search..."
+              inputProps={{ "aria-label": "search" }}
+            />
           </Search>
         </Stack>
         <Stack spacing={1}>
           <Stack direction={"row"} alignItems={"center"} spacing={1.5}>
             <ArchiveBox size={24} />
-            <Button>
-              Archive
-            </Button>
+            <Button>Archive</Button>
           </Stack>
 
           <Divider />
-
         </Stack>
-        <Stack spacing={2} direction={"column"} sx={{ flexGrow: 1, overflow: "auto", height: "100%" }}>
-          <SimpleBar style={{ maxHeight: '100%' }}> {/* Use SimpleBar for custom scrollbar */}
+        <Stack
+          spacing={2}
+          direction={"column"}
+          sx={{ flexGrow: 1, overflow: "auto", height: "100%" }}
+        >
+          <SimpleBar style={{ maxHeight: "100%" }}>
+            {" "}
+            {/* Use SimpleBar for custom scrollbar */}
             <Stack spacing={2.4}>
               <Typography variant="subtitle2" sx={{ color: "#676767" }}>
                 Pinned
               </Typography>
               {ChatList.filter((el) => el.pinned).map((el) => {
-                return <ChatElement key={el.id} {...el} />
+                return <ChatElement key={el.id} {...el} />;
               })}
             </Stack>
-            
             <Stack spacing={2.4}>
-              <Typography variant="subtitle2" sx={{ color: "#676767" ,pt:2}} >
+              <Typography variant="subtitle2" sx={{ color: "#676767", pt: 2 }}>
                 All Chats
               </Typography>
               {ChatList.filter((el) => !el.pinned).map((el) => {
-                return <ChatElement key={el.id} {...el} />
+                return <ChatElement key={el.id} {...el} />;
               })}
             </Stack>
-
           </SimpleBar>
         </Stack>
       </Stack>
